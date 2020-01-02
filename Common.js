1298a01921a97276d40ae24bb5000616f6788f94
zhuge <zhuge_lam@163.com>
2020-01-02 15:18:02 +0800

    添加手机号、身份证验证方法

--- Common.js
+++ Common.js
@@ -57,7 +57,56 @@ function Toast(msg, icon, duration, callback) {
     });
 }
 
+/**
+ * 判断字符串是否在数组中
+ * @param stringToSearch
+ * @param arrayToSearch
+ * @returns {boolean}
+ */
+function In_array(stringToSearch, arrayToSearch) {
+    for (var s = 0; s < arrayToSearch.length; s++) {
+        var thisEntry = arrayToSearch[s].toString();
+        if (thisEntry == stringToSearch) {
+            return true;
+        }
+    }
+    return false;
+}
+/**
+ * 验证手机正则
+ */
+function Mobile(mobile, type) {
+    type = type || true
+    if (type && !mobile) {
+        Common.Toast('请输入手机号码');
+        return false;
+    }
+    if (!(/^(0|86|17951)?1[0-9]{10}$/.test(mobile))) {
+        Toast('请输入正确的手机号码');
+        return false;
+    }
+    return true;
+}
+/**
+ * 验证身份证号正则
+ */
+function ID_number(id, type) {
+    type = type || true
+    if (type && !id) {
+        Common.Toast('请输入身份证号码');
+        return false;
+    }
+    if (!(/^[1-9]\d{5}(18|19|20|(3\d))\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/.test(id))) {
+        Toast('请输入正确的身份证号码');
+        return false;
+    }
+    return true;
+}
+
 module.exports = {
     Model: Model, // 对话框
     Toast: Toast, // 提示框
+    In_array: In_array,
+    ID_number: ID_number,
+    Mobile: Mobile,
 };
